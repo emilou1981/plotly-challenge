ca386c1a2d386bfbf87fe15d63cd2faa5f457b68
Emily Fischer <emily.m.fischer@outlook.com>
2019-11-02 11:55:08 -0500

    updates to graphs

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,7 +1,8 @@
 function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
-  var metaData = "/metadata/${sample}";
+  var metaData = `/metadata/${sample}`;
+  
   // d3.json(url).then(function(data) {
   //   console.log(data);
   // });
@@ -12,47 +13,47 @@ function buildMetadata(sample) {
 
     // Use `.html("") to clear any existing metadata
     sampleMetaData.html("");
-    // tbody.html("");
+ 
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
-    // Object.entries(userInfo).forEach(([key, value]) => console.log(`Key: ${key} and Value ${value}`));
-
-    // Object.entries(frequencyCounts).forEach(([key, value]) => {
-    //   var li = output.append("li").text(`${key}: ${value}`);
-
-
-    // Object.entries(weatherReport).forEach(function([key, value]) {
-    //   console.log(key, value);
-    //   // Append a cell to the row for each value
-    //   // in the weather report object
-    //   var cell = row.append("td");
-    //   cell.text(value);
+    
 
     Object.entries(sample).forEach(function ([key, value]) {
-      // console.log(key, value);
+      console.log(key, value);
       // Append a cell to the row for each value
       // in the weather report object
       var row = sampleMetaData.append("p");
-      row.text("${key}:${value}");
+      row.text(`${key}:${value}`);
     })
   });
 }
 
 
 
-// BONUS: Build the Gauge Chart
+// // BONUS: Build the Gauge Chart
 // buildGauge(data.WFREQ);
+// var data = [
+// 	{
+// 		domain: { x: [0, 1], y: [0, 1] },
+// 		value: 270,
+// 		title: { text: "Speed" },
+// 		type: "indicator",
+// 		mode: "gauge+number"
+// 	}
+// ];
 
+// var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+// Plotly.newPlot(gd, data, layout);
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  var Plot = "/samples/${sample}";
+  var plottingData = `/samples/${sample}`;
   // @TODO: Build a Bubble Chart using the sample data
-  d3.json(url).then(function (data) {
+  d3.json(plottingData).then(function (data) {
 
     // Grab values from the data json object to build the plots
     var xAxis = data.otu_ids;
@@ -73,17 +74,40 @@ function buildCharts(sample) {
       }
     };
 
-    var data = [bubble];
+    var bubbleData = [bubble];
     var layout = {
-      xaxis: { title: "OTU ID" }
+      xaxis: { title: "OTU ID"}
     };
-    Plotly.newPlot("bubble", data, layout);
+    Plotly.newPlot("bubble", bubbleData, layout);
 
     // @TODO: Build a Pie Chart
 
+
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-  }
+
+//     // Slices first two names
+// const left = names.slice(0, 2);
+// console.log(left);
+
+    // d3.json(plottingData).then(function(data){
+      console.log(data);
+      var sampleValues = data.sample_values.slice(0,10);
+      var labels = data.otu_ids.slice(0,10);
+      var display = data.otu_labels.slice(0,10);
+
+
+      var pie_chart = [{
+        values: sampleValues,
+        labels: labels,
+        hovertext: display,
+        type: "pie"
+      }];
+      Plotly.newPlot('pie',pie_chart);
+    });
+  // });
+};
+
 
 
   function init() {
